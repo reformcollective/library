f5aa32aa5d2ae6073e8c844a14cba89f224b027c
Robbie <robbiethewadley@gmail.com>
2023-02-01 14:02:10 -0700

    custom text overflow util

---
+++ CustomTextOverflow.tsx
@@ -0,0 +1,124 @@
+import { useRef } from "react"
+
+import useAnimation from "library/useAnimation"
+
+interface CustomTextOverflowProps {
+  /**
+   * the text to display
+   */
+  children: string
+  /**
+   * the custom ellipsis to display,
+   * defaults to "..."
+   */
+  ellipsis?: string
+  /**
+   * the number of lines you need
+   */
+  maxLines?: number
+  /**
+   * where in the text to place the ellipsis
+   * as an index in characters. negative numbers count from the end of the text,
+   * and 0 is the very end of the text
+   */
+  truncatePosition?: number
+}
+
+/**
+ * given a number of lines, this component will truncate the text to fit within
+ * the given number of lines.
+ *
+ * This can also adjust the overflow ellipsis to be any text you want, and can truncate at
+ * any position in the text (ie, the beginning, middle, or end of the line)
+ */
+export default function CustomTextOverflow({
+  children,
+  ellipsis = "...",
+  maxLines = 1,
+  truncatePosition = -1,
+}: CustomTextOverflowProps) {
+  const wrapperRef = useRef<HTMLDivElement>(null)
+
+  /**
+   * measure the number of lines taken up by the text, and recursively
+   * shorten the text until it fits within the given number of lines
+   */
+  useAnimation(
+    () => {
+      const wrapper = wrapperRef.current
+      if (!wrapper) return
+      const cleanup = () => {
+        wrapper.innerText = `${children}${ellipsis}`
+      }
+      const lines = getNumberOfLines(wrapperRef.current)
+
+      if (lines <= maxLines) {
+        wrapper.innerText = children
+        return cleanup
+      }
+
+      // recursively shrink the text until it fits
+      const shrinkText = (numberOfCharsToRemove: number) => {
+        // where we shrink the text needs to change if the truncate position is pos/neg
+        const shrinkBefore = truncatePosition < 0
+        // we need to use slightly different math to put the ellipses at the very end
+        const ellipsisAtEnd = truncatePosition === 0
+        const textBeforeEllipsis = ellipsisAtEnd
+          ? children.slice(0, children.length - numberOfCharsToRemove)
+          : children.slice(
+              0,
+              truncatePosition - (shrinkBefore ? numberOfCharsToRemove : 0)
+            )
+        const textAfterEllipsis = ellipsisAtEnd
+          ? ""
+          : children.slice(
+              truncatePosition + (shrinkBefore ? 0 : numberOfCharsToRemove)
+            )
+
+        const newText = `${textBeforeEllipsis}${ellipsis}${textAfterEllipsis}`
+
+        wrapper.innerText = newText
+        if (getNumberOfLines(wrapper) <= maxLines) return
+        if (numberOfCharsToRemove >= children.length) return
+        shrinkText(numberOfCharsToRemove + 1)
+      }
+
+      shrinkText(1)
+
+      return cleanup
+    },
+    [children, ellipsis, maxLines, truncatePosition],
+    {
+      recreateOnResize: true,
+    }
+  )
+
+  // prompt a CSS solution if possible
+  if (ellipsis === "..." && truncatePosition === 0)
+    return (
+      <>
+        This component should only be used if you need a custom ellipsis or
+        custom truncation position. Otherwise, use CSS.
+      </>
+    )
+  return (
+    <div ref={wrapperRef}>
+      {children}
+      {/* include the ellipsis for measurement purposes */}
+      {ellipsis}
+    </div>
+  )
+}
+
+/**
+ * get the number of lines that fit within the given element
+ * @param element the element to measure
+ * @returns the number of lines that would fit within the element
+ */
+const getNumberOfLines = (element: HTMLElement) => {
+  const { height } = element.getBoundingClientRect()
+  const lineHeight = parseFloat(
+    window.getComputedStyle(element).getPropertyValue("line-height")
+  )
+  return Math.ceil(height / lineHeight)
+}
