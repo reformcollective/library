51bc5ea067adf87345fdbfce881bf2ba16fc9ca7
Robbie <robbiethewadley@gmail.com>
2023-05-15 13:59:43 -0600

    add client only util

---
+++ ClientOnly.tsx
@@ -0,0 +1,21 @@
+import { useState, useEffect, startTransition } from "react"
+
+import { isBrowser } from "./functions"
+
+export default function ClientOnly({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    startTransition(() => {
+      setMounted(isBrowser())
+    })
+  }, [])
+
+  if (!mounted) return null
+
+  return <>{children}</>
+}
