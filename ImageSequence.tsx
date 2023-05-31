00a1cf80dbe4cc95b48527612570616f1fcea1e8
Robbie <robbiethewadley@gmail.com>
2023-05-31 16:19:29 -0600

    increase priority on image sequence

--- ImageSequence.tsx
+++ ImageSequence.tsx
@@ -181,6 +181,7 @@ export default function ImageSequence({
                 scrub: 0.5,
                 start: triggerStart ?? "top top",
                 end: triggerEnd ?? "bottom top",
+                refreshPriority: 999,
               }
             : undefined,
         }
