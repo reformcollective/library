4b7998c10e7fc08107cb6777b9c9e890e2cf1a4b
Robbie <robbiethewadley@gmail.com>
2023-08-31 15:45:28 -0600

    tweak animation

--- AutoAnimate.tsx
+++ AutoAnimate.tsx
@@ -1,16 +1,20 @@
-import { usePrevious, useThrottle } from "ahooks"
+import { useDeepCompareEffect, usePrevious, useThrottle } from "ahooks"
 import { gsap } from "gsap"
-import { useLayoutEffect, useRef } from "react"
+import { useRef } from "react"
 import styled from "styled-components"
+
+import useAnimation from "./useAnimation"
 
 export default function AutoAnimate({
   children,
   duration = 1,
   skipFirstAnimation = true,
+  parameters,
 }: {
   children: React.ReactNode
   duration?: number
   skipFirstAnimation?: boolean
+  parameters?: GSAPTweenVars
 }) {
   const currentTime = useThrottle(children, {
     wait: duration * 1100,
@@ -22,35 +26,32 @@ export default function AutoAnimate({
   const current = useRef<HTMLDivElement | null>(null)
   const firstRender = useRef(true)
 
-  useLayoutEffect(() => {
-    if (firstRender.current && skipFirstAnimation) {
-      firstRender.current = false
-      return
-    }
+  useAnimation(
+    () => {
+      if (firstRender.current && skipFirstAnimation) {
+        firstRender.current = false
+        return
+      }
 
-    gsap.fromTo(
-      previous.current,
-      {
-        yPercent: 0,
-      },
-      {
+      gsap.to(previous.current, {
         yPercent: -100,
         ease: "power3.inOut",
         duration,
-      },
-    )
-    gsap.fromTo(
-      current.current,
-      {
+        ...parameters,
+      })
+      gsap.from(current.current, {
         yPercent: 100,
-      },
-      {
-        yPercent: 0,
         ease: "power3.inOut",
+        ...parameters,
         duration,
-      },
-    )
-  }, [lastTime, currentTime, duration, skipFirstAnimation])
+      })
+    },
+    [duration, parameters, skipFirstAnimation],
+    {
+      extraDeps: [currentTime, lastTime],
+      effect: useDeepCompareEffect,
+    },
+  )
 
   return (
     <Wrapper>
