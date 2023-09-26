97e3b27e544cafd86bea3aeb138d761bcee50faf
Robbie <robbiethewadley@gmail.com>
2023-09-26 11:49:54 -0600

    create lazy video component

---
+++ LazyVideo.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react"
+import Lazy from "vanilla-lazyload"
+
+const loader = new Lazy()
+
+type Props = {
+  poster: string
+  className?: string
+  style?: React.CSSProperties
+} & (
+  | { sourceMP4: string; sourceWEBM?: string }
+  | { sourceMP4?: string; sourceWEBM: string }
+)
+
+export const LazyVideo = ({
+  className,
+  poster,
+  style,
+  sourceMP4,
+  sourceWEBM,
+}: Props) => {
+  useEffect(() => {
+    loader.update()
+  }, [])
+
+  return (
+    <video
+      style={style}
+      className={`lazy ${className ?? ""}`}
+      data-src={sourceMP4}
+      data-poster={poster}
+      autoPlay
+      muted
+      loop
+      playsInline
+    >
+      {sourceWEBM && <source src={sourceWEBM} type="video/webm" />}
+      {sourceMP4 && <source src={sourceMP4} type="video/mp4" />}
+    </video>
+  )
+}
