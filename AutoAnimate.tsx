19020e6e7370945d9026da8c51c26822f90e27ce
Robbie <robbiethewadley@gmail.com>
2024-04-18 13:14:54 -0600

    format css

--- AutoAnimate.tsx
+++ AutoAnimate.tsx
@@ -170,8 +170,8 @@ const Wrapper = styled.div<{
 
   > * {
     grid-area: 1 / 1 / 2 / 2;
-	width: 100%;
-	height: 100%;
+    width: 100%;
+    height: 100%;
 
     &:empty {
       pointer-events: none;
