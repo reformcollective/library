c1c3c4821a8a8dea9971bfbd42f3eb7ba1ee940d
max <maxdoylemason@gmail.com>
2024-07-30 09:36:03 -0600

    autoHideHeader hook

--- useAutoHideHeader.ts
+++ useAutoHideHeader.ts
@@ -1,7 +1,6 @@
 import { ScrollSmoother } from "gsap/ScrollSmoother"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
-import { type RefObject, useState } from "react"
-import useAnimation from "./useAnimation"
+import { type RefObject, useState, useRef, useEffect } from "react"
 
 const isElementInViewport = (element: HTMLElement | null) => {
 	const rect = element?.getBoundingClientRect()
@@ -22,14 +21,28 @@ const checkElementsInViewport = (elementIds: string[]) => {
 	return false
 }
 
+/**
+ * A custom hook that controls header visibility based on scroll position and specified elements.
+ *
+ * @param wrapper The ref object of the header element.
+ * @param hideHeaderElementIds An array of element IDs that will hide the header when in view.
+ * @param stickyHeaderElementIds An array of element IDs that will make the header sticky when in view.
+ * @returns The current translateY value for the header, which allows for vertical positioning.
+ */
 export default function useAutoHideHeader(
 	wrapper: RefObject<HTMLDivElement>,
 	hideHeaderElementIds: string[] = [],
 	stickyHeaderElementIds: string[] = [],
 ) {
 	const [translateY, setTranslateY] = useState(0)
+	const translateYRef = useRef(0)
 
-	useAnimation(() => {
+	// Update translateYRef whenever the state variables changes.
+	useEffect(() => {
+		translateYRef.current = translateY
+	}, [translateY])
+
+	useEffect(() => {
 		let lastScroll = 0
 
 		ScrollTrigger.create({
@@ -39,6 +52,7 @@ export default function useAutoHideHeader(
 				lastScroll = scroll
 				const height = wrapper.current?.offsetHeight ?? 0
 
+				// Check for elements included in the hideHeaderElementIds and stickyHeaderElementIds arrays
 				const shouldHideHeader = checkElementsInViewport(hideHeaderElementIds)
 				const shouldStickyHeader = checkElementsInViewport(
 					stickyHeaderElementIds,
@@ -48,10 +62,18 @@ export default function useAutoHideHeader(
 					setTranslateY(-height)
 				} else if (shouldStickyHeader) {
 					setTranslateY(0)
-				} else if (delta > 0) {
-					setTranslateY((prev) => Math.max(-height, prev - delta))
 				} else {
-					setTranslateY((prev) => Math.min(0, prev - delta))
+					if (scroll === 0) {
+						translateYRef.current = 0
+					} else if (delta > 0) {
+						translateYRef.current = Math.max(
+							-height,
+							translateYRef.current - delta,
+						)
+					} else {
+						translateYRef.current = Math.min(0, translateYRef.current - delta)
+					}
+					setTranslateY(translateYRef.current)
 				}
 			},
 		})
