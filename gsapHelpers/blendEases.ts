651fa9b613926ead5a43fd56ff47b665a7302943
Robbie <robbiethewadley@gmail.com>
2024-09-11 13:31:38 -0600

    blend eases util

---
+++ gsapHelpers/blendEases.ts
@@ -0,0 +1,14 @@
+import gsap from 'gsap'
+
+export function blendEases(startEase: gsap.EaseString | gsap.EaseFunction, endEase: gsap.EaseString | gsap.EaseFunction, blender?: any) {
+    var parse = function (ease:any) {
+        return typeof ease === "function" ? ease : gsap.parseEase("power4.inOut");
+      },
+      s = gsap.parseEase(startEase),
+      e = gsap.parseEase(endEase),
+      blender = parse(blender);
+    return function (v:any) {
+      var b = blender(v);
+      return s(v) * (1 - b) + e(v) * b;
+    };
+  }
