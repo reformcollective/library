03e7ff4cb65f283532ccd690154110137f824fd5
Robbie <robbiethewadley@gmail.com>
2024-09-18 11:52:54 -0600

    Add SVG and string support to UniversalImage

--- UniversalImage.tsx
+++ UniversalImage.tsx
@@ -1,21 +1,27 @@
 import type { IGatsbyImageData } from "gatsby-plugin-image"
 import { GatsbyImage } from "gatsby-plugin-image"
 import type { ComponentProps } from "react"
+import styled from "styled-components"
 
-type GatsbyImageProps = ComponentProps<typeof GatsbyImage>
+type DefaultImageProps = ComponentProps<typeof GatsbyImage> &
+	ComponentProps<"img">
 
 export type UniversalImageData =
 	| IGatsbyImageData
 	| { childImageSharp: { gatsbyImageData: IGatsbyImageData | null } | null }
+	| { gatsbyImageData: IGatsbyImageData | null }
+	| { file: { url: string | null } | null }
+	| string
 	| null
 	| undefined
 
-export type UniversalImageProps = Omit<GatsbyImageProps, "image"> & {
+export type UniversalImageProps = Omit<DefaultImageProps, "image" | "src"> & {
 	image: UniversalImageData
 }
 
 export default function UniversalImage({
 	image,
+	loading = "lazy",
 	...props
 }: UniversalImageProps) {
 	if (!image) {
@@ -23,9 +29,22 @@ export default function UniversalImage({
 		return null
 	}
 
-	const imageToUse =
-		"childImageSharp" in image ? image.childImageSharp?.gatsbyImageData : image
-	return imageToUse ? (
-		<GatsbyImage loading="lazy" image={imageToUse} {...props} />
-	) : null
+	if (typeof image === "string") return <Image src={image} {...props} />
+
+	if ("file" in image)
+		return image.file?.url ? <Image src={image.file.url} {...props} /> : null
+
+	if ("childImageSharp" in image)
+		return image.childImageSharp?.gatsbyImageData ? (
+			<GatsbyImage image={image.childImageSharp.gatsbyImageData} {...props} />
+		) : null
+
+	if ("gatsbyImageData" in image)
+		return image.gatsbyImageData ? (
+			<GatsbyImage image={image.gatsbyImageData} {...props} />
+		) : null
+
+	return <GatsbyImage image={image} {...props} />
 }
+
+const Image = styled.img``
