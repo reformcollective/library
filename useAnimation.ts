5a4ad37535e817cbdbaabe01d755ca419aee30fd
Dallen Hoyal <dallen.hoyal@gmail.com>
2024-10-15 11:49:50 -0600

    added svgelement

--- useAnimation.ts
+++ useAnimation.ts
@@ -35,7 +35,7 @@ const useAnimation = <F, T>(
 	createAnimations: F extends VoidFunction ? F : () => T,
 	deps: DependencyList,
 	options?: {
-		scope?: React.RefObject<HTMLElement | null>
+		scope?: React.RefObject<HTMLElement | SVGElement |null>
 		kill?: boolean
 		recreateOnResize?: boolean
 		extraDeps?: DependencyList
