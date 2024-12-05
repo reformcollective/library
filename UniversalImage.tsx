2adb981ae011e349e30808fd6a7f1f0b0528bdad
Robbie <robbiethewadley@gmail.com>
2024-12-05 11:03:18 -0700

    improve types

--- UniversalImage.tsx
+++ UniversalImage.tsx
@@ -1,13 +1,13 @@
 import type { SanityImageCrop, SanityImageHotspot } from "@/sanity.types"
 import { dataset, projectId } from "@/sanity/lib/api"
 import Image, { type StaticImageData } from "next/image"
-import type { ImgHTMLAttributes } from "react"
 import { SanityImage } from "sanity-image"
 import { styled } from "./styled"
+import type { ImgHTMLAttributes } from "react"
 
 type DefaultImageProps = Omit<
 	ImgHTMLAttributes<HTMLImageElement>,
-	"placeholder" | "onLoad" | "src" | "srcSet" | "width" | "height" | "image"
+	"src" | "width" | "height"
 >
 
 export type SanityImageData = {
@@ -24,40 +24,31 @@ export type UniversalImageData =
 	| null
 	| undefined
 
-export type UniversalImageProps = Omit<DefaultImageProps, "image" | "src"> & {
+export type UniversalImageProps = DefaultImageProps & {
 	src: UniversalImageData
 	alt: string
 	objectFit?: string
 	objectPosition?: string
+	width?: number
+	height?: number
 }
 
 export default function UniversalImage({ src, ...props }: UniversalImageProps) {
 	if (!src) return null
 
 	if (typeof src === "string") {
-		return <Image src={src} {...props} />
+		return <Image {...props} src={src} />
 	}
 
 	if ("_type" in src) {
 		if (!src.asset) return null
 		return (
 			<DefaultSanityImage
-				hotspot={
-					src.hotspot?.x && src.hotspot.y
-						? { x: src.hotspot.x, y: src.hotspot.y }
-						: undefined
-				}
-				crop={
-					src.crop?.top && src.crop.left && src.crop.bottom && src.crop.right
-						? {
-								top: src.crop.top,
-								left: src.crop.left,
-								bottom: src.crop.bottom,
-								right: src.crop.right,
-							}
-						: undefined
-				}
 				{...props}
+				// @ts-expect-error library type mismatch
+				hotspot={src.hotspot}
+				// @ts-expect-error library type mismatch
+				crop={src.crop}
 				id={src.asset?._ref}
 				projectId={projectId}
 				dataset={dataset}
@@ -65,7 +56,7 @@ export default function UniversalImage({ src, ...props }: UniversalImageProps) {
 		)
 	}
 
-	return <Image src={src} {...props} />
+	return <Image {...props} src={src} />
 }
 
 const DefaultSanityImage = styled(
