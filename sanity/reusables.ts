58a530b17f1a40eca93d26eda698fc32bf024254
Robbie <robbiethewadley@gmail.com>
2024-12-11 13:58:17 -0700

    utility fixes and tweaks

--- sanity/reusables.ts
+++ sanity/reusables.ts
@@ -2,21 +2,25 @@ import { defineField } from "sanity"
 
 export const imageWithAlt = ({
 	name,
+	required,
 	title,
 }: {
 	name: string
 	title?: string
+	required?: boolean
 }) =>
 	defineField({
 		type: "image",
 		name,
 		title,
+		validation: required ? (rule) => rule.required() : undefined,
 		fields: [
 			defineField({
 				type: "text",
 				name: "alt",
 				title: "Alternative text",
 				rows: 2,
+				validation: (rule) => rule.required(),
 			}),
 		],
 		options: {
