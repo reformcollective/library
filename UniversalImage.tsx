32f96105033ed86bff326a1c992ba4c573b97073
Robbie <robbiethewadley@gmail.com>
2024-12-13 15:40:18 -0700

    better initialization strategy

--- UniversalImage.tsx
+++ UniversalImage.tsx
@@ -2,10 +2,15 @@
 
 import { dataset, projectId } from "@/sanity/lib/api"
 import Image, { type StaticImageData } from "next/image"
-import type { ImgHTMLAttributes } from "react"
+import { use, createContext, type ImgHTMLAttributes } from "react"
 import { SanityImage } from "sanity-image"
 import type { SanityImageData } from "./sanity/imageMetadata"
 import { styled } from "./styled"
+
+export const eagerContext = createContext(false)
+export const EagerImages = ({ children }: { children: React.ReactNode }) => (
+	<eagerContext.Provider value={true}>{children}</eagerContext.Provider>
+)
 
 type DefaultImageProps = Omit<
 	ImgHTMLAttributes<HTMLImageElement>,
@@ -38,7 +43,13 @@ export default function UniversalImage({
 	...otherProps
 }: UniversalImageProps) {
 	if (!src) return null
-	const props = { objectFit, alt, ...otherProps }
+	const defaultEager = use(eagerContext)
+	const props = {
+		objectFit,
+		alt,
+		loading: defaultEager ? ("eager" as const) : ("lazy" as const),
+		...otherProps,
+	}
 
 	if (typeof src === "string") {
 		return <DefaultImage {...props} src={src} />
