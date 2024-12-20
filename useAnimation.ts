1b14fd571496bb314900596b0c334141d5cdaeb3
Robbie <robbiethewadley@gmail.com>
2024-12-20 11:21:27 -0700

    format

--- useAnimation.ts
+++ useAnimation.ts
@@ -1,9 +1,8 @@
 import { type ContextSafeFunc, useGSAP } from "@gsap/react"
-import gsap, { ScrollTrigger } from "gsap/all"
+import gsap from "gsap/all"
 import type { DependencyList } from "react"
-import { use, useDeferredValue, useEffect, useState } from "react"
-import { createDebouncedEventListener, ScreenContext } from "./ScreenContext"
-import { isBrowser } from "./deviceDetection"
+import { use, useDeferredValue, useState } from "react"
+import { ScreenContext } from "./ScreenContext"
 
 let globalRefresh: ReturnType<typeof setTimeout> | undefined
 
