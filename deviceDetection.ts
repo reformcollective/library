a1d1cc52d5b61136fd86b4037e4652d35fa71ec5
Robbie <robbiethewadley@gmail.com>
2024-12-31 19:45:47 -0700

    fix window access

--- deviceDetection.ts
+++ deviceDetection.ts
@@ -3,8 +3,10 @@
 import { useEffect, useState } from "react"
 
 // sanity will run in JSDOM, so check for that to ensure we're not running extra code for schema scripts
-const isJSDOM = window.navigator.userAgent.toLowerCase().includes("jsdom")
-export const isBrowser = typeof window !== "undefined" || !isJSDOM
+const isJSDOM =
+	typeof window !== "undefined" &&
+	window.navigator.userAgent.toLowerCase().includes("jsdom")
+export const isBrowser = typeof window !== "undefined" && !isJSDOM
 
 export const isIOS = () => {
 	if (!isBrowser) return false
