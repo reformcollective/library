f3dc574b142ddd856f518300806a72453eaf3a3f
Robbie <robbiethewadley@gmail.com>
2025-03-03 15:06:33 -0700

    case sensitive

--- sanity/PortableText.tsx
+++ sanity/PortableText.tsx
@@ -1,4 +1,4 @@
-import UniversalLink from "library/loader/UniversalLink"
+import UniversalLink from "library/new-loader/UniversalLink"
 import { styled } from "library/styled"
 import {
 	PortableText,
