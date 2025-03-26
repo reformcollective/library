b43efac201dfb76cc163f698e3ab0d9fda0de768
Robbie <robbiethewadley@gmail.com>
2025-03-26 16:07:19 -0600

    video timing fix

--- LazyVideo.tsx
+++ LazyVideo.tsx
@@ -2,7 +2,7 @@
 
 import { ScrollTrigger } from "gsap/all"
 import { type ReactNode, type Ref, useRef, useState } from "react"
-
+import { usePreloader } from "./link/usePreloader"
 import { useAnimation } from "./useAnimation"
 import useCombinedRefs from "./useCombinedRefs"
 
@@ -45,16 +45,46 @@ export function LazyVideo({
 	const [showVideo, setShowVideo] = useState(false)
 	const videoRef = useCombinedRefs(ref)
 	const hasStartedLoading = useRef(false)
+	const { completed } = usePreloader()
 
-	useAnimation(() => {
-		return ScrollTrigger.create({
-			trigger: videoRef.current,
-			start: "top-=200 bottom",
-			end: "bottom+=200 top",
-			onEnter: () => setShowVideo(true),
-			onEnterBack: () => setShowVideo(true),
-		})
-	}, [videoRef])
+	useAnimation(
+		() => {
+			if (!completed) return
+			if (showVideo) return
+
+			const show = (self: ScrollTrigger) => {
+				if (!videoRef.current) return
+
+				if (self.start < 0 || self.end < 0) {
+					// video not loaded
+				} else {
+					setShowVideo(true)
+				}
+			}
+
+			// for whatever reason, gsap does a very bad job of tracking videos
+			// when the don't have a source. so I'm computing the values
+			// manually here, which is slightly less efficient but works much better
+			ScrollTrigger.create({
+				trigger: videoRef.current,
+				start: () => {
+					if (!videoRef.current) return Number.NEGATIVE_INFINITY
+					const bounds = videoRef.current.getBoundingClientRect()
+					const scroll = window.scrollY
+					return scroll - window.innerHeight + bounds.top - 200
+				},
+				end: () => {
+					if (!videoRef.current) return Number.NEGATIVE_INFINITY
+					const bounds = videoRef.current.getBoundingClientRect()
+					return `+=${bounds.height + 400}`
+				},
+				onEnter: show,
+				onEnterBack: show,
+			})
+		},
+		[videoRef, completed, showVideo],
+		{ recreateOnResize: true },
+	)
 
 	return (
 		<video
