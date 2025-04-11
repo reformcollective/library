60a5175e3705a7e8186761eefc0681893eb32f54
Robbie <robbiethewadley@gmail.com>
2025-04-11 11:44:42 -0600

    only check site URL during next builds

--- siteURL/index.ts
+++ siteURL/index.ts
@@ -2,8 +2,10 @@
  * if you want to manually specify a URL, specify NEXT_PUBLIC_DEPLOY_URL or update your next config
  */
 
+const isNext = !!process.env.NEXT_RUNTIME
 const environmentURL = process.env.NEXT_PUBLIC_DEPLOY_URL
-if (!environmentURL)
+
+if (isNext && !environmentURL)
 	throw new Error("NEXT_PUBLIC_DEPLOY_URL is not set in next config")
 
-export const siteURL = environmentURL
+export const siteURL = environmentURL || "http://localhost:3000"
