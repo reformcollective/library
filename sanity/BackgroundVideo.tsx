e4226987839adecdca2a06768b99112183c72426
Robbie <robbiethewadley@gmail.com>
2025-04-25 10:23:12 -0600

    move video helper into library

---
+++ sanity/BackgroundVideo.tsx
@@ -0,0 +1,54 @@
+"use client"
+
+import MuxVideo from "@mux/mux-video-react"
+import type { AssetMeta } from "./assetMetadata"
+import { useCombinedRefs } from "library/useCombinedRefs"
+import { useEffect, useRef, useState } from "react"
+
+export function BackgroundVideo({
+	data,
+	...props
+}: {
+	/**
+	 * asset metadata from sanity
+	 */
+	data: AssetMeta | undefined
+	loop?: boolean
+	className?: string
+	ref?: React.Ref<HTMLVideoElement>
+}) {
+	const [playbackFailedAtWidth, setPlaybackFailedAtWidth] = useState<
+		false | number
+	>(false)
+	const video = useRef<HTMLVideoElement>(null)
+
+	const roundedWidth = playbackFailedAtWidth
+		? Math.floor(playbackFailedAtWidth / 100) * 100
+		: playbackFailedAtWidth
+
+	useEffect(() => {
+		video.current
+			?.play()
+			.catch(() => setPlaybackFailedAtWidth(window.innerWidth))
+	}, [])
+
+	return (
+		<MuxVideo
+			{...props}
+			ref={useCombinedRefs(video, props.ref)}
+			playbackId={
+				// once the video has failed, don't try to play it again
+				playbackFailedAtWidth ? undefined : data?.playbackId
+			}
+			muted
+			playsInline
+			poster={
+				roundedWidth
+					? `https://image.mux.com/${data?.playbackId}/thumbnail.webp?time=${data?.videoDuration}&width=${roundedWidth}`
+					: data?.videoBlurUrl
+			}
+			style={{ aspectRatio: data?.videoAspectRatio }}
+			streamType="on-demand"
+		/>
+	)
+}
