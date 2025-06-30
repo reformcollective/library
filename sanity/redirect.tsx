b4e5c33d41842f634b4a4097015ba77f8bce2fd7
Robbie <robbiethewadley@gmail.com>
2025-06-30 13:36:15 -0600

    add redirect section

---
+++ sanity/redirect.tsx
@@ -0,0 +1,43 @@
+import { css, f, styled } from "library/styled"
+import { draftMode } from "next/headers"
+import { redirect } from "next/navigation"
+import type { GetSectionType } from "page"
+import colors from "styles/colors"
+import textStyles from "styles/text"
+
+export async function Redirect({ link }: GetSectionType<"redirect">) {
+	const isDraft = await (await draftMode()).isEnabled
+
+	if (isDraft) {
+		return (
+			<Wrapper>
+				<p>
+					This page is configured as a redirect. If someone tries to navigate to
+					this page in any way, they will be redirected to{" "}
+					<a href={link}>{link}</a>
+				</p>
+				<br />
+				<p>You are seeing this message because you are in draft mode.</p>
+			</Wrapper>
+		)
+	}
+
+	if (!link) return null
+	redirect(link)
+}
+
+const Wrapper = styled("div", {
+	...f.unresponsive(css`
+		grid-column: main;
+		height: 100vh;
+		padding: 100px;
+		background: white;
+		color: black;
+		text-align: center;
+		overflow-wrap: break-word;
+
+		a {
+			text-decoration: underline;
+		}
+	`),
+})
