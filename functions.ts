c0eb8d85158413b6d564d08ae3e84ad011a4f107
Robbie <robbiethewadley@gmail.com>
2025-12-02 12:59:53 -0700

    add getRandomFloat

--- functions.ts
+++ functions.ts
@@ -43,6 +43,10 @@ export const getRandomInt = (min: number, max: number) => {
 	return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
+export const getRandomFloat = (min: number, max: number) => {
+	return Math.random() * (max - min) + min
+}
+
 export function measureSticky<T>(
 	element: Element | null | undefined,
 	value: T,
