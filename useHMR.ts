818805a2f2b58cf482bd3e2db6efc2a5fecddcd6
Robbie <robbiethewadley@gmail.com>
2025-12-17 15:41:49 -0700

    only useHMR in dev

--- useHMR.ts
+++ useHMR.ts
@@ -6,24 +6,26 @@ const socket =
 		? new WebSocket("ws://localhost:3000/_next/webpack-hmr")
 		: null
 
-export const useHMR = (callback: (hash: string) => void) => {
-	const latestCallback = useLatest(callback)
+export const useHMR =
+	process.env.NODE_ENV === "development"
+		? (callback: (hash: string) => void) => {
+				const latestCallback = useLatest(callback)
 
-	useEffect(() => {
-		if (process.env.NODE_ENV === "development") {
-			const handler = (event: MessageEvent) => {
-				const message = JSON.parse(event.data) as
-					| { type: "unknown" }
-					| { type: "built"; hash: string }
-				if (message.type === "built") {
-					console.log("HMR hash updated:", message.hash)
-					latestCallback.current(message.hash)
-				}
+				useEffect(() => {
+					if (process.env.NODE_ENV === "development") {
+						const handler = (event: MessageEvent) => {
+							const message = JSON.parse(event.data) as
+								| { type: "unknown" }
+								| { type: "built"; hash: string }
+							if (message.type === "built") {
+								latestCallback.current(message.hash)
+							}
+						}
+						socket?.addEventListener("message", handler)
+						return () => {
+							socket?.removeEventListener("message", handler)
+						}
+					}
+				}, [latestCallback])
 			}
-			socket?.addEventListener("message", handler)
-			return () => {
-				socket?.removeEventListener("message", handler)
-			}
-		}
-	}, [latestCallback])
-}
+		: () => {}
