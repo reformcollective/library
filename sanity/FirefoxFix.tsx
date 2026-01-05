a385768cc0cda828d91436b9c3da6d690aa5cc87
Robbie <robbiethewadley@gmail.com>
2026-01-05 12:14:35 -0700

    automatic stega cleaning

--- sanity/FirefoxFix.tsx
+++ sanity/FirefoxFix.tsx
@@ -1,9 +1,16 @@
 "use client"
 
 import { useInterval } from "ahooks"
+import { browserData } from "library/deviceDetection"
 import { useHMR } from "library/useHMR"
+import { stegaClean } from "next-sanity"
+import { useDraftModePerspective } from "next-sanity/hooks"
+import { toast } from "sonner"
 
 let hasWarned = false
+
+// biome-ignore lint/suspicious/noMisleadingCharacterClass: directly copied from sanity
+const zeroWidthChars = /[\u200B\u200C\u200D\uFEFF]/g
 
 const checkZeroWidthChars = (startNode: Node = document.body) => {
 	if (process.env.NODE_ENV === "development") {
@@ -12,21 +19,21 @@ const checkZeroWidthChars = (startNode: Node = document.body) => {
 
 			if (node.nodeType === Node.TEXT_NODE) {
 				const parent = node.parentElement
-				// biome-ignore lint/suspicious/noMisleadingCharacterClass: from sanity
-				const zeroWidthChars = /[\u200B\u200C\u200D\uFEFF]/g
 				const hasStega = zeroWidthChars.test(node.textContent ?? "")
 
 				if (parent && hasStega) {
 					const computedStyle = getComputedStyle(parent)
 					const letterSpacing = parseFloat(computedStyle.letterSpacing)
 
+					// remove the stega and replace the content
 					if (letterSpacing < 0) {
-						hasWarned = true
-						console.error(
-							"Steganography detected with negative letter spacing!",
-							"You MUST use stegaClean() to remove the zero width characters.",
-							parent,
-						)
+						const cleanContent = stegaClean(node.textContent ?? "")
+						node.textContent = cleanContent
+
+						if (!hasWarned) {
+							hasWarned = true
+							toast.info("Some draft mode features are disabled in Firefox")
+						}
 					}
 				}
 			} else {
@@ -34,16 +41,19 @@ const checkZeroWidthChars = (startNode: Node = document.body) => {
 			}
 		}
 
-		if (!hasWarned) walk(startNode)
+		walk(startNode)
 	}
 }
 
 export const FirefoxFix = () => {
+	const perspective = useDraftModePerspective()
+
 	useInterval(() => {
-		checkZeroWidthChars()
+		if (browserData.isFireFox && perspective === "drafts") checkZeroWidthChars()
 	}, 1000)
 	useHMR("postbuild", () => {
 		hasWarned = false
+		checkZeroWidthChars()
 	})
 
 	return null
