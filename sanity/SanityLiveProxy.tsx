1058dba05617f5eb250ed292204204a833a288c3
Robbie <robbiethewadley@gmail.com>
2026-01-06 09:28:30 -0700

    format

--- sanity/SanityLiveProxy.tsx
+++ sanity/SanityLiveProxy.tsx
@@ -7,7 +7,7 @@ import { useEffect } from "react"
 
 /**
  * a lightweight version of SanityLive
- * 
+ *
  * - shares connections to sanity between clients
  * - works event without CORS configuration (although you should still configure it for the studio)
  */
