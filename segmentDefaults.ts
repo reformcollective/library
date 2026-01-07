a13f5760f3db435312dd3e1dcb5f1d6446ec53cd
Robbie <robbiethewadley@gmail.com>
2026-01-06 17:07:57 -0700

    restore segment defaults

---
+++ segmentDefaults.ts
@@ -0,0 +1,11 @@
+// dynamic	'auto' | 'force-dynamic' | 'error' | 'force-static'	'auto'
+export const dynamic = "force-static"
+
+// dynamicParams	boolean	true
+export const dynamicParams = true
+
+// revalidate	false | 0 | number	false
+export const revalidate = false
+
+// fetchCache	'auto' | 'default-cache' | 'only-cache' | 'force-cache' | 'force-no-store' | 'default-no-store' | 'only-no-store'	'auto'
+export const fetchCache = "only-cache"
