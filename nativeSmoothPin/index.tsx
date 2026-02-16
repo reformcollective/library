f7fbb37b2cbc7902d8bc8d738f551ecf855dda1c
Robbie <robbiethewadley@gmail.com>
2026-02-16 11:42:52 -0700

    add warning logs

--- nativeSmoothPin/index.tsx
+++ nativeSmoothPin/index.tsx
@@ -5,7 +5,7 @@ const css = String.raw
 export default function nativeSmoothPin({
 	goopLevel = 200,
 	goopType = "both",
-	top = "0",
+	top: topInput = "0",
 	containerAware = false,
 }: {
 	goopLevel?: number
@@ -13,6 +13,21 @@ export default function nativeSmoothPin({
 	top: string
 	containerAware?: boolean
 }) {
+	const top = Number.parseFloat(topInput) === 0 ? "0.1px" : topInput
+
+	const negativeTop = Number.parseFloat(top) <= 0
+	if (negativeTop && !containerAware) {
+		throw new Error(
+			"nativeSmoothPin: top must be greater than 0 when containerAware is false",
+		)
+	}
+
+	if (negativeTop) {
+		console.warn(
+			"nativeSmoothPin: top is less than or equal to 0, nativeSmoothPin will not work if your sticky element is taller than the viewport",
+		)
+	}
+
 	return css`
 		top: ${top};
 
