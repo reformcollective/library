7a8befd10554507b3771e04e514a9fcdf40719a3
Robbie <robbiethewadley@gmail.com>
2026-03-12 14:14:10 -0600

    Update ViewTransition.tsx

--- ViewTransition.tsx
+++ ViewTransition.tsx
@@ -1,11 +1,11 @@
 "use client"
 
-// biome-ignore lint/correctness/noUnusedImports: required to load type definitions
-import {} from 'react/canary'
 import {
 	type ComponentProps,
 	ViewTransition as ReactViewTransition,
 } from "react"
+// biome-ignore lint/correctness/noUnusedImports: required to load type definitions
+import {} from "react/canary"
 import { usePreloader } from "./link/usePreloader"
 import { type CSSObject, createStyle } from "./styled"
 
