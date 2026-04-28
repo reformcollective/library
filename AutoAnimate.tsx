46b06ba482bcfbf9f2d2758f1ad0434ed1b3bd3d
RJWadley <robbiethewadley@gmail.com>
2026-04-28 17:09:41 -0600

    fix margin collapse in AutoAnimate

--- AutoAnimate.tsx
+++ AutoAnimate.tsx
@@ -297,9 +297,13 @@ export default function AutoAnimate({
 
 	return (
 		<Wrapper className={className}>
-			<div ref={sizer} style={{ display: "none" }}>
-				{getNodeFromKey(currentKey)}
-			</div>
+			<AnimationWrapper
+				ref={sizer}
+				alignment={alignment}
+				style={{ display: "none" }}
+			>
+				<div>{getNodeFromKey(currentKey)}</div>
+			</AnimationWrapper>
 			<AnimationWrapper ref={wrapper} alignment={alignment}>
 				<div ref={wrapperA}>{getNodeFromKey(slotA)}</div>
 				<div ref={wrapperB}>{getNodeFromKey(slotB)}</div>
