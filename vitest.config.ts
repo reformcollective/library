bc00bec1610515402a00996fbe144218ee4c35b6
RJWadley <robbiethewadley@gmail.com>
2026-06-10 16:29:48 -0600

    test: resolve tsconfig paths

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,8 @@
+import viteTsconfigPaths from "vite-tsconfig-paths"
 import { defineConfig } from "vitest/config"
 
 export default defineConfig({
+	plugins: [viteTsconfigPaths({ projects: ["../tsconfig.json"] })],
 	test: {
 		typecheck: {
 			enabled: true,
