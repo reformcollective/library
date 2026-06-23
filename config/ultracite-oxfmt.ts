48bf27268b0f74a9eb97b442b25a9bc2aeadfbb6
RJWadley <robbiethewadley@gmail.com>
2026-06-23 13:41:15 -0600

    add oxfmt

--- config/ultracite-oxfmt.ts
+++ config/ultracite-oxfmt.ts
@@ -1,25 +1,13 @@
 import { defineConfig } from "oxfmt"
 import ultracite from "ultracite/oxfmt"
 
-export default defineConfig({
-	...ultracite,
-	printWidth: 100,
-	semi: false,
-	sortPackageJson: false,
-	trailingComma: "all",
-	// Oxfmt resolves ignorePatterns from the root config file location.
-	// Root apps should re-export this helper from their own root config.
-	ignorePatterns: [...ultracite.ignorePatterns, "pnpm-lock.yaml", "library/**"],
-	useTabs: true,
-	sortImports: {
-		groups: [
-			"type-import",
-			["value-builtin", "value-external"],
-			"type-internal",
-			"value-internal",
-			["type-parent", "type-sibling", "type-index"],
-			["value-parent", "value-sibling", "value-index"],
-			"unknown",
-		],
-	},
-})
+export function createOxfmtConfig() {
+	return defineConfig({
+		...ultracite,
+		semi: false,
+		useTabs: true,
+		trailingComma: "all",
+	})
+}
+
+export default createOxfmtConfig()
